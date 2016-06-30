8a6993c6d453a17f926d332a6afe04b71ba0d9f2
Viktor.Mironov <vicktor.mironov@gmail.com>
2016-06-30 23:14:52 +0500

    Добавлена валидация параметров бинарной операции

--- Calculator/Operations/DivideDouble.cs
+++ Calculator/Operations/DivideDouble.cs
@@ -8,16 +8,29 @@ namespace Calculator.Operations
 
         public double Calculate(double first, double second)
         {
+            if (IsAnyInvalid(first, second))
+                throw new ArgumentException("Некорректное значение аргумента операции");
+            if(IsAnyTooBigOrSmall(first, second))
+                throw new ArgumentOutOfRangeException("Аргумент операции вышел за пределы допустимых значений", (Exception)null);
+
             var result = first/second;
 
             if (IsWasDivideByZero(second, result))
                 throw new DivideByZeroException("Деление на ноль");
-
             if (TooBigAbsoluteValue(result))
                 throw new OverflowException("Результат вычисления имеет слишком большое значение по модулю");
 
-
             return result;
+        }
+
+        private static bool IsAnyTooBigOrSmall(double first, double second)
+        {
+            return double.IsInfinity(first) || double.IsInfinity(second);
+        }
+
+        private static bool IsAnyInvalid(double first, double second)
+        {
+            return double.IsNaN(first) || double.IsNaN(second);
         }
 
         private bool IsWasDivideByZero(double second, double result)
--- Calculator/Operations/SummarizeDouble.cs
+++ Calculator/Operations/SummarizeDouble.cs
@@ -6,12 +6,27 @@ namespace Calculator.Operations
     {
         public double Calculate(double first, double second)
         {
+            if (IsAnyInvalid(first, second))
+                throw new ArgumentException("Некорректное значение аргумента операции");
+            if (IsAnyTooBigOrSmall(first, second))
+                throw new ArgumentOutOfRangeException("Аргумент операции вышел за пределы допустимых значений", (Exception)null);
+
             var result = first + second;
 
             if (TooBigAbsoluteValue(result))
                 throw new OverflowException("Результат вычисления имеет слишком большое значение по модулю");
 
             return result;
+        }
+
+        private static bool IsAnyTooBigOrSmall(double first, double second)
+        {
+            return double.IsInfinity(first) || double.IsInfinity(second);
+        }
+
+        private static bool IsAnyInvalid(double first, double second)
+        {
+            return double.IsNaN(first) || double.IsNaN(second);
         }
 
         private static bool TooBigAbsoluteValue(double result)
