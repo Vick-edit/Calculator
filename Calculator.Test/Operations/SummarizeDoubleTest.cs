cf1bc4854ccb8cb080df7e00f79d7fb10be015cb
Viktor.Mironov <vicktor.mironov@gmail.com>
2016-06-30 19:30:20 +0500

    "Красный тест"

---
+++ Calculator.Test/Operations/SummarizeDoubleTest.cs
@@ -0,0 +1,23 @@
+﻿using Calculator.Operations;
+using NUnit.Framework;
+
+namespace Calculator.Test.Operations
+{
+    [TestFixture]
+    public class SummarizeDoubleTest
+    {
+        [TestCase(94.86, 147.45, 242.31)]
+        public void Calculate_SumTwoPosistiv_CorrectPositivReturned(double firstNumber, double secondNumber, double correctSum)
+        {
+            //arrange
+            var summarizer = new SummarizeDouble();
+
+            //act
+            var result = summarizer.Calculate(firstNumber, secondNumber);
+
+            //assert
+            Assert.That(result, Is.Positive);
+            Assert.That(result, Is.EqualTo(correctSum));
+        }
+    }
+}
