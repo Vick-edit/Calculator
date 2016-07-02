9fd3c5ba21e8365592cb1e96e29ae736dee77212
Viktor.Mironov <vicktor.mironov@gmail.com>
2016-07-02 17:30:15 +0500

    Добавлено перечисление, сопоставляющее операцию и её приоритет

---
+++ Calculator/Operations/BaseBinaryOperationsPriority.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.Operations
+{
+    public enum BaseBinaryOperationsPriority
+    {
+        Multiplication = 1,
+        Division = 2,
+        Subtraction = 3,
+        Summation = 4
+    }
+}
