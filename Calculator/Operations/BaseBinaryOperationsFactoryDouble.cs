cb34004ed19474b7e7d90ee06e76fce6a5b4982f
Viktor.Mironov <vicktor.mironov@gmail.com>
2016-07-02 17:24:52 +0500

    Добавлен строитель базовых бинарных операций работающих с double

---
+++ Calculator/Operations/BaseBinaryOperationsFactoryDouble.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator.Operations
+{
+    public class BaseBinaryOperationsFactoryDouble : IBaseBinaryOperationsFactory<double>
+    {
+        public IBinaryOperation<double> GetMultiplier()
+        {
+            return new MultiplyDouble();
+        }
+
+        public IBinaryOperation<double> GetDivider()
+        {
+            return new DivideDouble();
+        }
+
+        public IBinaryOperation<double> GetSubtractor()
+        {
+            return new SubtractDouble();
+        }
+
+        public IBinaryOperation<double> GetSummarizer()
+        {
+            return new SummarizeDouble();
+        }
+    }
+}
---
+++ Calculator/Operations/Interfaces/IBaseBinaryOperationsBulider.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.Operations
+{
+    public interface IBaseBinaryOperationsFactory<T> where T : struct
+    {
+        IBinaryOperation<T> GetMultiplier();
+        IBinaryOperation<T> GetDivider();
+        IBinaryOperation<T> GetSubtractor();
+        IBinaryOperation<T> GetSummarizer();
+    }
+}
