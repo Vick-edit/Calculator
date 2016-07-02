b79f8b25eec610ddf9fd3e20ea30e8c5dd38d31a
Viktor.Mironov <vicktor.mironov@gmail.com>
2016-07-02 17:10:34 +0500

    Добавлены интерфейс и класс – обёртка, совмещает бинарную операцию и набор символ, определяющий её

---
+++ Calculator/OperationContainers/BinaryOperationContainer.cs
@@ -0,0 +1,16 @@
+﻿using Calculator.Operations;
+
+namespace Calculator.Containers
+{
+    public class BinaryOperationContainer<T> : IBinaryOperationContainer<T> where T : struct 
+    {
+        public string[] Literals { get; private set; }
+        public IBinaryOperation<T> Operation { get; private set; }
+
+        public BinaryOperationContainer(string[] literals, IBinaryOperation<T> operation)
+        {
+            Literals = literals;
+            Operation = operation;
+        }
+    }
+}
---
+++ Calculator/OperationContainers/IBinaryOperationContainer.cs
@@ -0,0 +1,10 @@
+﻿using Calculator.Operations;
+
+namespace Calculator.Containers
+{
+    public interface IBinaryOperationContainer<T> where T: struct 
+    {
+        string[] Literals { get; }
+        IBinaryOperation<T> Operation { get; } 
+    }
+}
