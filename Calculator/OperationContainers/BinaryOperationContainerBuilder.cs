b0723613b0989029e870bea9ad7447aa41105d67
Viktor.Mironov <vicktor.mironov@gmail.com>
2016-07-02 17:42:27 +0500

    Добавлен строитель контейнеров для операций

---
+++ Calculator/OperationContainers/BinaryOperationContainerBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Calculator.Operations;
+
+namespace Calculator.OperationContainers
+{
+    public class BinaryOperationContainerBuilder<T> : IBinaryOperationContainerBuilder<T> where T : struct 
+    {
+        public IBinaryOperationContainer<T> BuildContainer(string[] literals, IBinaryOperation<T> operation)
+        {
+            return new BinaryOperationContainer<T>(literals, operation);
+        }
+    }
+}
---
+++ Calculator/OperationContainers/Interfaces/IBinaryOperationContainerBuilder.cs
@@ -0,0 +1,9 @@
+﻿using Calculator.Operations;
+
+namespace Calculator.OperationContainers
+{
+    public interface IBinaryOperationContainerBuilder<T> where T : struct
+    {
+        IBinaryOperationContainer<T> BuildContainer(string[]literals, IBinaryOperation<T> operation);
+    }
+}
