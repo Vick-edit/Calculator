85f63fe1d8f749c596e9a2cdeba43c0e9ca51b9f
Viktor.Mironov <vicktor.mironov@gmail.com>
2016-07-03 13:11:06 +0500

    Реализация интерфейса строителя бинарного ядра математики

--- Calculator/MathCore/BinaryMathInDoubleBuilder.cs
+++ Calculator/MathCore/BinaryMathInDoubleBuilder.cs
@@ -1,10 +1,18 @@
-﻿namespace Calculator.MathCore
+﻿using System.Dynamic;
+using Calculator.OperationContainers;
+using Calculator.Operations;
+
+namespace Calculator.MathCore
 {
     public class BinaryMathInDoubleBuilder : IBinaryMathBuilder<double>
     {
         public IBinaryPartsOfMath<double> GetBinaryMathCore()
         {
-            throw new System.NotImplementedException();
+            var operationsFactory = new BaseBinaryOperationsFactoryDouble();
+            var operationsContainerBuilder = new BinaryOperationContainerBuilder<double>();
+
+            var mathCore = new BinaryPartsOfMath<double>(operationsFactory, operationsContainerBuilder);
+            return mathCore;
         }
     }
 }
