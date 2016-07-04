d442803a8430c7cbae14af3bff64fe99eed64418
Viktor.Mironov <vicktor.mironov@gmail.com>
2016-07-04 20:14:23 +0500

    Реализация класса, который проходит тесты для парсинга выражений со скобками

--- Calculator/Parsers/ParserRoundBrackets.cs
+++ Calculator/Parsers/ParserRoundBrackets.cs
@@ -5,9 +5,24 @@ using Sprache;
 namespace Calculator.Parsers
 {
     public class ParserRoundBrackets<T> : ParserBinaryOperations<T>, IParserRoundBrackets<T> where T : struct 
-    {        
-        public Parser<Expression> ExpressionInBrackets { get; private set; }
-        
+    {
+        public Parser<Expression> ExpressionInBrackets
+        {
+            get
+            {
+                return 
+                    from leftBracket in Parse.Char('(')
+					from expr in OrderedExpressions
+					from rightBracket in Parse.Char(')')
+					select expr;
+            }
+        }
+
+        public override Parser<Expression> Operand
+        {
+            get { return ExpressionInBrackets.XOr(base.Operand); }
+        }
+
         public ParserRoundBrackets(IParserNumber<T> numberParser, IBinaryOperationMap<T> binaryMapper) 
             : base(numberParser, binaryMapper)
         {
